5c57977658b6a1ced5bf1f925bfafaa5601905b9
lai2-24 <lai2-24@itismeucci.com>
2025-10-08 09:21:39 +0200

    Fixed

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,36 +5,46 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws UnknownHostException, IOException {
-        System.out.println("Hello world!");
-        System.out.println("inserisci");
-        
+    public static void main(String[] args) throws IOException {
         Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter ip");
 
-        String ip = myObj.nextLine();
-        System.out.println("inserisci porta del server");
-        int porta = myObj.nextInt();
-        Socket mioSocket = new Socket(ip,porta);
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
-        PrintWriter out = new PrintWriter(mioSocket.getOutputStream(), true);
         
-        
-        String stringaDaInviare = myObj.nextLine();
-        System.out.println("inserisci la stringa");
-        String stringadainviare = myObj.nextLine();
-        out.println(stringadainviare);
+            System.out.println("Inserire IP Server:");
+            String ip = myObj.nextLine();
 
-        String stringClient = in.readLine();
+            System.out.println("Inserire porta del server:");
+            int porta = myObj.nextInt();
+            myObj.nextLine();
 
-        System.out.println("la stringa ricevuta dal server e: " + stringClient);
-   
-   
+            Socket mioSocket = new Socket(ip, porta);
+            BufferedReader in = new BufferedReader(new InputStreamReader(mioSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(mioSocket.getOutputStream(), true);
+
+            System.out.println("Connesso al server; premere EXIT per uscire");
+
+            while (true) {
+                System.out.print("Inserire il messaggio: ");
+                String messageToSend = myObj.nextLine();
+
+                if (messageToSend.equalsIgnoreCase("EXIT")) {
+                    out.println("!"); 
+                    break;
+                } else {
+                    out.println(messageToSend);
+                }
+
+                String serverResponse = in.readLine();
+                System.out.println("Risposta server: " + serverResponse);
+            }
+
+            in.close();
+            out.close();
+            mioSocket.close();
+            myObj.close();
+            System.out.println("Connessione terminata");
     }
-
 }
+
